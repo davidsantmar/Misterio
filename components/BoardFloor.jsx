3a60829e45c01516ccc02e55e33a4acd98bc35d9
David <davidsantmar@gmail.com>
2025-09-26 15:23:30 +0200

    Possibilities added

--- components/BoardFloor.jsx
+++ components/BoardFloor.jsx
@@ -10,11 +10,13 @@ export function BoardFloor({ diceValue }) {
     'Creepster-Regular': require('../assets/fonts/Creepster-Regular.ttf'),
   });
   const [position, setPosition] = useState(0); 
-  const [possibility, setPossibility] = useState(0);
-  //const [stored, setStored] = useState(0);
+ 
+  const [buttonColor, setButtonColor] = useState('lightblue');
+  const [room1Color, setRoom1Color] = useState('white');
+  const [room2Color, setRoom2Color] = useState('white');
+  const [room3Color, setRoom3Color] = useState('white');
+  const [room4Color, setRoom4Color] = useState('white');
   const router = useRouter();
-  
-
   // Lista de casillas con sus contenidos para facilitar la gestión
   const board = [
     { content: <><ForwardArrow size={24} /><Text style={styles.stoneText}>Planta baja</Text></> },
@@ -48,42 +50,62 @@ export function BoardFloor({ diceValue }) {
     { content: null },
     { content: <><Text style={styles.stoneText}>Planta baja</Text><BackArrow size={24} /></> },
   ];
-  const [colors, setColors] = useState(board.map(() => '#4682B4')); // Color inicial para cada stone
-    const [borderColors, setBorderColors] = useState(board.map(() => 'black')); // Color inicial para cada stone
+  const [colors, setColors] = useState(board.map(() => '#808080')); // Color inicial para cada stone
+  const [borderColors, setBorderColors] = useState(board.map(() => 'black')); // Color inicial para cada stone
 
-  // Mostrar posibilidades de movimiento
-  const showPossibilities = (index) => {
-    const newColors = [...colors];
-    // Cambiar el color del stone clicado (por ejemplo, alternar entre dos colores)
-    newColors[index] = newColors[index] === '#4682B4' ? '#FF6347' : '#4682B4';
-    setColors(newColors); // Actualizar el estado
-  }
+  const updateBorderColors = (storedValue, diceValue) => {
+    setBorderColors((prevColors) => {
+      const newColors = [...prevColors]; // Crear una copia del arreglo
+      const sumIndex = Number(storedValue) + Number(diceValue);
+      const diffIndex = Number(storedValue) - Number(diceValue);
+
+      // Validar que los índices sean válidos
+      if (sumIndex >= 0 && sumIndex < newColors.length) {
+        newColors[sumIndex] = newColors[sumIndex] === 'black' ? 'yellow' : 'black';
+      }
+      if (diffIndex >= 0 && diffIndex < newColors.length) {
+        // Usar la misma condición que en sumIndex, si es intencional
+        newColors[diffIndex] = newColors[diffIndex] === 'black' ? 'yellow' : 'black';
+      }
+
+      return newColors; // Devolver el nuevo arreglo para actualizar el estado
+    });
+  };
  
   const toDiceRoll = () => {
     router.push('/dice');
-    //setPosition(position + Number(diceValue));    
-    //storePlayerPosition((position + Number(diceValue)).toString()); //posicion + dado
+  
   };
   useEffect(() => { //la suma y guardado funciona bien
     const fetchPosition = async () => {
-      const storedValue = await getPlayerPosition();
-      console.log('Fetched stored value:', storedValue);
-      setPosition(storedValue !== null ? Number(storedValue) : 0);
-      const newColors = [...colors];
+    const storedValue = await getPlayerPosition();
+    console.log('Fetched stored value:', storedValue);
+    //setPosition(storedValue !== null ? Number(storedValue) : 0);
+    const newColors = [...colors];
     // aqui se encuentra el jugador
-    newColors[storedValue] = newColors[storedValue] === '#4682B4' ? '#FF6347' : '#4682B4';
-    borderColors[Number(storedValue) + (Number(diceValue))] = borderColors[Number(storedValue) + (Number(diceValue))] === 'black' ? 'yellow' : 'black';
-
-    setColors(newColors); // Actualizar el estado
-    console.log(Number(storedValue) + (Number(diceValue)))
-      //return storedValue;
-    }
-    fetchPosition();
-    //position + dado debe iluminarse
-    console.log('position + diceValue', position + Number(diceValue)) 
-    //position vuelve a 0 tras pasar por dado :(
+    newColors[storedValue] = newColors[storedValue] === '#FF6347' ? '#808080' : '#FF6347';
     
-
+    updateBorderColors(storedValue, diceValue);
+    
+    setColors(newColors); // Actualizar el estado
+    console.log('position:', storedValue)
+    console.log('possibilityUp:', Number(storedValue) + (Number(diceValue)))
+    console.log('possibilityDown:', Number(storedValue) - (Number(diceValue)))
+    // Resaltar habitaciones si el jugador puede entrar en ellas
+    if ((storedValue <= 4 && Number(storedValue) + (Number(diceValue)) > 4) || (storedValue >= 4 && Number(storedValue) - (Number(diceValue)) < 4)){
+      setRoom1Color('yellow');
+    }
+    if ((storedValue <= 12 && Number(storedValue) + (Number(diceValue)) > 12) || (storedValue >= 12 && Number(storedValue) - (Number(diceValue)) < 12)){
+      setRoom3Color('yellow');
+    }
+    if ((storedValue <= 21 && Number(storedValue) + (Number(diceValue)) > 21) || (storedValue >= 21 && Number(storedValue) - (Number(diceValue)) < 21)){
+      setRoom2Color('yellow');
+    }
+    if ((storedValue <= 27 && Number(storedValue) + (Number(diceValue)) > 27) || (storedValue >= 27 && Number(storedValue) - (Number(diceValue)) < 27)){
+      setRoom4Color('yellow');
+    }
+  }
+  fetchPosition();
   }, []);
   const getPlayerPosition = async () => {
     try {
@@ -101,14 +123,14 @@ export function BoardFloor({ diceValue }) {
         console.log('error saving data');
     }
   };
+  
 
 
    
   const stoneClicked = (index) => { //se guarda la position al clickar
-    //setPosition(index)
-    console.log (index) //number
-
+    setPosition(index)
     storePlayerPosition(index.toString()); 
+    setButtonColor('lightgreen');
     //Tira dados a continuación
    
   }
@@ -117,8 +139,8 @@ export function BoardFloor({ diceValue }) {
       
       <View style={styles.container}>
         <View style={styles.leftRoomsContainer}>
-          <ImageBackground style={styles.room1} source={require('../assets/images/boardImages/Labo.png')} />
-          <ImageBackground style={styles.room2} source={require('../assets/images/boardImages/Library.png')} />
+          <ImageBackground style={[styles.room1, { borderColor: room1Color}]} source={require('../assets/images/boardImages/Labo.png')} />
+          <ImageBackground style={[styles.room2, { borderColor: room2Color}]} source={require('../assets/images/boardImages/Library.png')} />
         </View>
         <View style={styles.stonesContainer}>
           {board.map((stone, index) => (
@@ -132,11 +154,11 @@ export function BoardFloor({ diceValue }) {
           ))}
         </View>
         <View style={styles.rightRoomsContainer}>
-          <Pressable style={styles.buttonContainer} onPress={toDiceRoll}>
+          <Pressable style={[styles.buttonContainer, { backgroundColor: buttonColor }]} onPress={toDiceRoll}>
               <Text style={styles.buttonText}>Dados</Text>
             </Pressable>
-          <ImageBackground style={styles.room3} source={require('../assets/images/boardImages/Lounge.png')} />
-          <ImageBackground style={styles.room4} source={require('../assets/images/boardImages/Bedroom.png')} />
+          <ImageBackground style={[styles.room3, { borderColor: room3Color}]} source={require('../assets/images/boardImages/Lounge.png')} />
+          <ImageBackground style={[styles.room4, { borderColor: room4Color}]} source={require('../assets/images/boardImages/Bedroom.png')} />
         </View>
       </View>
     </ScrollView>
@@ -165,6 +187,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 1,
     justifyContent: 'center',
+    borderWidth: 4,
   },
   stoneText: {
     fontFamily: 'Creepster-Regular',
@@ -177,11 +200,13 @@ const styles = StyleSheet.create({
     height: 300,
     width: 150,
     marginBottom: 600,
+        borderWidth: 4,
   },
   room2: {
     height: 300,
     width: 150,
     marginBottom: 150,
+        borderWidth: 4,
   },
   rightRoomsContainer: {
     flexDirection: 'column',
@@ -191,11 +216,14 @@ const styles = StyleSheet.create({
     height: 300,
     width: 150,
     marginTop: 300,
+        borderWidth: 4,
+
   },
   room4: {
     height: 300,
     width: 150,
     marginTop: 400,
+        borderWidth: 4,
   },
   buttonContainer: {
     backgroundColor: 'lightblue',
